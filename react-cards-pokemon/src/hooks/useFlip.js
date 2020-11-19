86605b7e2b5a79b51a499913c3115bef92ac79bb
Jedediah Arnold <jarnold910@gmail.com>
2020-11-19 18:14:14 -0500

    steps one and two

---
+++ react-cards-pokemon/src/hooks/useFlip.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const useFlip = () => {
+    const [state, setState] = useState(true);
+
+    const toggleState = () => {
+        setState(state => !state)
+    }
+
+    return [state, toggleState];
+}
+
+export default useFlip;
