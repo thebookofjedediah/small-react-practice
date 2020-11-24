b9010c709b7bfd176fb8456ffb59c6fb4915a12c
Jedediah Arnold <jarnold910@gmail.com>
2020-11-23 21:29:56 -0500

    added joke functional to class based

---
+++ react-jokes-classes/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import JokeList from "./JokeList";
+import JokeListClass from './JokeListClass'
+
+function App() {
+  return (
+    <div className="App">
+      <JokeList />
+      <hr />
+      <JokeListClass />
+    </div>
+  );
+}
+
+export default App;
